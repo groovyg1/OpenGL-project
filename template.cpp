6d545d8f459b46b2e54045be07e9606df3e5f696
Gopi Shankar <gpshankar16@gmail.com>
2017-02-09 15:11:30 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,26 +1,29 @@
 #include <GL/GLUT.h>
 
 void Draw() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
+	glClear(GL_COLOR_BUFFER_BIT);		// Clear Buffer to preset value, parameter is mask
+	glFlush();				// force execution of GL commands in finite time
 }
 
 
 void Initialize() {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	glClearColor(1.0, 1.0, 1.0, 0.0);	// Have fun Changing the color of the windows
+	glMatrixMode(GL_PROJECTION);		//* specify which matrix is the current matrix
+	glLoadIdentity();			//* replace the current matrix with the identity matrix
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0); //* multiply the current matrix with an orthographic matrix
 }
 
 int main(int iArgc, char** cppArgv) {
-	glutInit(&iArgc, cppArgv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(950, 500);
-	glutInitWindowPosition(200, 200);
-	glutCreateWindow("Hello World");
-	//Initialize();
-	glutDisplayFunc(Draw);
-	glutMainLoop();
+	glutInit(&iArgc, cppArgv);			// Initialize GLUT Library
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);	// Initialize Display and set display mode
+	glutInitWindowSize(950, 500);			// Set Windows Size
+	glutInitWindowPosition(200, 200);		// Set Windows Position
+	glutCreateWindow("Hello World");		// Set Windows Title
+	//Initialize();		
+	glutDisplayFunc(Draw);				// Display Function takes function as argument - (sets the display callback for the current window.)
+	glutMainLoop();					// Holds the Screen as long as the 'X' button is clicked
 	return 0;
 }
+
+
+//* these have not been understood
